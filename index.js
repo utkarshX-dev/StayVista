5c10e5135ed824f2018da47d52df2ea41c6d9c4f
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-06-09 11:12:24 +0530

    user section

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 const dbUrl = process.env.ATLASDB_URL;
-
 main()
 .then(() => {
   console.log("MongoDB connected!");
